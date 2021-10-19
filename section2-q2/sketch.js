2e65e2ca4c0f4b60c3556c0f700aacbf38685180
oxonigg99 <m.gg.nctone.ni30@outlook.jp>
2021-10-19 13:11:39 +0900

    section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -7,15 +7,15 @@ function setup() {
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
-      if ((i+j)%2!=0) {
-        fill(128,128,128);
-        rect(size*i,size*j,size,size);
+      if ((i+j)%2!=0) {//i+tが2でわりきれない。else＝2で割り切れるは白空きなので省略
+        fill(128,128,128);//グレー塗りつぶし
+        rect(size*i,size*j,size,size);//長方形かく
       }
 
 
-      if((i+j)%2!=0&&j>=0&&j<3){
-        fill(255,0,0);
-        ellipse(size*(i+1/2),size*(j+1/2),size*6/7)
+      if((i+j)%2!=0&&j>=0&&j<3){//↑の条件勝つj：y座標が0，1．2
+        fill(255,0,0);//赤塗りつぶし
+        ellipse(size*(i+1/2),size*(j+1/2),size*6/7)//中心が四角の真ん中、直径四角の6/7の円
       }
 
       if((i+j)%2!=0&&j>=5&&j<8){
