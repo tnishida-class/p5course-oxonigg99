0f836ae7cda9690669b4554615b37e365b5cc5ae
oxonigg99 <m.gg.nctone.ni30@outlook.jp>
2021-10-26 21:08:55 +0900

    section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -10,38 +10,32 @@ function setup(){
 
 function draw(){
   background(160, 192, 255);
-  count = (count + 1) % cycle;//12345・・て変数が順に変わる。周期的な動き、おきまりcycle=周期の長さ
-  // BLANK[1]
+  if(keyIsDown(" ".charCodeAt(0))){
+  count = (count + 3 ) % cycle;//12345・・て変数が順に変わる。周期的な動き、おきまりcycle=周期の長さ
+}
+else{
+    count = (count+1) % cycle;
+  }
+
+
+
   let size= count;
-//  if(count>0 && count<50){
-//  ellipse(width / 2, height / 2, size^2);
-  // fill(255,0,0);//
-// }
 
-//  if(count=50){
-  //ellipse(width / 2, height / 2, size*2);
-  // fill(255,0,0);//
-  //}
-
-  //if(count>50 && count<100){
-//  ellipse(width / 2, height / 2, (cycle-size)^2);
-  // fill(255,0,0);//
-//  }
-  if(count>0 && count<=40){
+  if(count>=0 && count<=40){
   ellipse(width / 2, height / 2, size/2+10);
-   fill(255,0,0);//
  }
 
   if(count>40 && count<=50){
-  ellipse(width / 2, height / 2, 3*(size)^2);
-     fill(255,0,0);//
+  ellipse(width / 2, height / 2, 2*(size)^2);
 }
+
 if(count>50 && count <=60){
-ellipse(width / 2, height / 2, 3*(cycle-size)^2);
-   fill(255,0,0);//
+ellipse(width / 2, height / 2, 2*(cycle-size)^2);
 }
+
 if(count>60 && count <=100){
  ellipse(width / 2, height / 2, (cycle-size)/2+10);
-   fill(255,0,0);//
 }
+fill(255,0,0);//
+
 }
