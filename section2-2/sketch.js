1b6401efb5fc5fa3ca86dceb360121b4843ab6af
oxonigg99 <m.gg.nctone.ni30@outlook.jp>
2021-10-27 14:14:27 +0900

    section2
    分からなくなってきたので原点に立ち返ろうと前々回の授業分をやりました

--- section2-2/sketch.js
+++ section2-2/sketch.js
@@ -7,16 +7,30 @@
 function setup() {
   createCanvas(100,100);
   background(196);
-  for(let i = 0; i < 9; i++){
+  for(let i=0;i<9;i=i+1){
+  let x=(i+1)*10
+  line(x,0,x,100);
+
+ if(i%2==0){
+  strokeWeight(2);
+ }
+
+ else{
+   strokeWeight(1)
+ }
+}
+
+
+//  for(let i = 0; i < 9; i++){
     // 変えるのはここから
-    if(i % 2 == 0){
-      strokeWeight(2);
-    }
-    else{
-      strokeWeight(1);
-    }
+  //  if(i % 2 == 0){
+  //    strokeWeight(2);
+  //  }
+  //  else{
+  //    strokeWeight(1);
+  //  }
     // ここまでの間になります
-    let x = i * 10 + 10;
-    line(x, 0, x, 100);
-  }
+  //  let x = i * 10 + 10;
+    //line(x, 0, x, 100);
+  //}
 }
