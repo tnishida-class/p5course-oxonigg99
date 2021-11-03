287f2e173ba621d8bb27b437446c70e543d538b3
oxonigg99 <m.gg.nctone.ni30@outlook.jp>
2021-11-03 18:05:44 +0900

    section4-2
    何もドラックされていないときにわらわらでてくるの全然できなかった

--- section4-2/sketch.js
+++ section4-2/sketch.js
@@ -3,6 +3,8 @@
 // 練習問題：何も操作しなくてもボールが湧いてくる機能を追加しよう
 
 let balls;
+let count;
+let x,y;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
@@ -16,8 +18,18 @@ function draw(){
     ellipse(b.x, b.y, b.size);
     b.x += b.vx;
     b.y += b.vy;
+  　b.size=random(1,40);
   }
 }
+　 count = (count+1) % cycle;
+  if(count>0 && count<20){
+    x=random(0,width)
+    y=random(0,height)
+    ellipse(x,y,size)
+  }
+
+
+
 
 function mouseDragged(){
   const dx = mouseX - pmouseX;
