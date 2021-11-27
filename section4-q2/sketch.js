2ca0983e5178da2521972737f203a7d60668ba7f
oxonigg99 <m.gg.nctone.ni30@outlook.jp>
2021-11-27 14:21:28 +0900

    section4-q2
    数字表示以外できた

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -1,18 +1,24 @@
-// テキスト「配列を使った描画」練習問題：円グラフ
-
-function setup(){
+function setup() {
   createCanvas(400, 400);
   background(240);
 
   // 配列をランダムに初期化する
   let scores = [];
-  for(let i = 0; i < 10; i++){
-    scores[i] = random(20, 100); // 60以上100未満のランダムな数を代入
-  }
+  for (let i = 0; i < 10; i++) {scores[i] = random(20, 100);} // 60以上100未満のランダムな数を代入
+
 
   // 円グラフを描くには割合が必要なので合計を計算しておく
   let total = 0;
-  for(let i = 0; i < scores.length; i++){ total += scores[i]; }
+  for (let i = 0; i < scores.length; i++) {
+    total += scores[i];
+  }
 
-  // BLANK[1]
+let start=0//くり返す前は0から(どこに描画されるかはわからない)スタートさせる(繰り返し始めたstartには前のエンドが代入される)
+for (let i = 0; i < scores.length; i++) {
+
+ let R =width*0.7;
+ let end =start+(scores[i]*TWO_PI)/total ;
+ arc(200,200,R,R,start,end,PIE);
+ start=end;
+  }
 }
